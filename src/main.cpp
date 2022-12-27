9beda2f1b8807c4af14b60f15ae2e0fb2e92cb80
John Mills <jkmv45@gmail.com>
2022-12-27 14:23:46 -0700

    Added matplotlibcpp for plotting data. Updated CMake files to include matplotlib and generate test exe in single call to cmake/make.

--- src/main.cpp
+++ src/main.cpp
@@ -2,28 +2,19 @@
 #include <vector>
 #include <array>
 
+#include "matplotlibcpp.h"
 #include "eigen3/Eigen/Geometry"
 
 #include "swarmAgent.hpp"
 #include "environmentManager.hpp"
 
+namespace plt = matplotlibcpp;
+
 int main() {
     std::cout << "Hello World!" << "\n";
 
-    // std::vector<SimObj*> test, test2;
-    // SwarmAgent* agent1 = new SwarmAgent();
-    // SwarmAgent* agent2 = new SwarmAgent();
-    // SwarmAgent* agent3 = new SwarmAgent();
-
-    // test.push_back(agent1);
-    // test.push_back(agent2);
-
-    // test2.push_back(agent1);
-    // test2.push_back(agent3);
-    // test2.push_back(agent2);
-
-    // std::cout << "Size Not Equal: " << (test.size() != test2.size()) << std::endl;
-    // std::cout << "Content Not Equal: " << (test != test2) << std::endl;
+    plt::plot({1,3,2,4});
+    plt::show();
 
     return 0;
 }
